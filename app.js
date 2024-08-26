b207de43da7f9fdb9ed2f88ad93a9242846af9bb
SyedZakariaRizvi <syedzakariarizvi@gmail.com>
2024-08-27 00:08:57 +0530

    Set up MongoDB connection and defined Mongoose model for Campground

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require("express")
 const path = require("path")
+const mongoose = require("mongoose")
+const Campground = require("./models/campground.js")
+require("dotenv").config()
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => { 
+        console.log("Database Connected")
+    })
+    .catch((err) => {
+        console.log("Error connecting to Database: ", err)
+    })
 
 const app = express()
 const PORT = 3000
@@ -9,6 +20,12 @@ app.set("views", path.join(__dirname, "views"))
 
 app.get("/", (req, res) => {
     res.render("home")
+})
+
+app.get("/makecampground", async (req, res) => {
+    const camp = new Campground({ title: "Camp One", description: "First campground" })
+    await camp.save()
+    res.send(camp)
 })
 
 app.listen(PORT, () => {
