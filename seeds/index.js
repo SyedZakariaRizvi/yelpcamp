5dd74a8d231a829a70212e4c55fa18f34eb4c8d5
SyedZakariaRizvi <syedzakariarizvi@gmail.com>
2024-08-29 20:25:38 +0530

    Update schema to include price, and update seeds to add description, image and price

--- seeds/index.js
+++ seeds/index.js
@@ -21,8 +21,12 @@ const seedDB = async () => {
     await Campground.deleteMany({})
     for(let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000)
+        const randomPrice = Math.floor(Math.random() * 30) + 10
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            price: randomPrice,
+            description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate tempora praesentium asperiores odio earum eaque, et aliquam? Voluptatem itaque, laudantium quis vitae eius in assumenda deserunt ipsam animi illum non?",
             location: `${cities[random1000].city}, ${cities[random1000].state}`
         })
         await camp.save()
