3ca44c44be390af1f0ec23ef7c661516dc11b553
SyedZakariaRizvi <syedzakariarizvi@gmail.com>
2024-09-03 23:53:31 +0530

    Setup express session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path")
 const mongoose = require("mongoose")
 const ejsMate = require("ejs-mate")
 const methodOverride = require("method-override")
+const session = require("express-session")
 require("dotenv").config()
 
 const ExpressError = require("./utils/ExpressError.js")
@@ -28,6 +29,18 @@ app.set("views", path.join(__dirname, "views"))
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
+
+const sessionConfig = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24,
+        maxAge: Date.now() + 1000 * 60 * 60 * 24
+    }
+}
+app.use(session(sessionConfig))
 
 app.use("/campgrounds", campgrounds)
 app.use("/campgrounds/:id/reviews", reviews)
